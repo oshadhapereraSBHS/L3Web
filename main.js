5b1c89ef74e774c88dd399b36e1bc630cb57f989
oshadhapereraSBHS <80649645+oshadhapereraSBHS@users.noreply.github.com>
2023-07-17 19:48:45 +1200

    arranged subheadings horizontally using divs under main nav bar

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 function signUpPage (){
     location.href = "signup.html";
 }
+
+function dropdown1 (){
+    document.getElementById("dropdownItems1").style.display = "block"
+    document.getElementById("overview").style.textDecoration = "underline"
+}
+function removeDropdown1 (){
+    document.getElementById("dropdownItems1").style.display = "none";
+    document.getElementById("overview").style.textDecoration = "none"
+
+}
+

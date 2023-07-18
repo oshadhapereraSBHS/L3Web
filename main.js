b2ab92661f364f85bd5b68710668166e70879686
oshadhapereraSBHS <80649645+oshadhapereraSBHS@users.noreply.github.com>
2023-07-18 13:33:17 +1200

    adding slideshow using array of slides, using i

--- main.js
+++ main.js
@@ -48,14 +48,14 @@ function openOverlay() {
     showSlides(slideIndex = n);
   }
   
-//   function showSlides(n) {
-//     var i;
-//     var slides = document.getElementsByClassName("slides");
-//     if (n > slides.length) {slideIndex = 1}
-//     if (n < 1) {slideIndex = slides.length}
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     slides[slideIndex-1].style.display = "block";
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
     
-//   }
+  }

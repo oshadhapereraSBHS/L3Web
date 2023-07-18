b3c843768167a041134cd42528dc9bc579c9fa6a
oshadhapereraSBHS <80649645+oshadhapereraSBHS@users.noreply.github.com>
2023-07-18 13:23:25 +1200

    changing slideIndex based on arrows pressed (right arrow adds 1, left subtracts 1)

--- main.js
+++ main.js
@@ -29,3 +29,33 @@ function removeDropdown2 (){
 }
 
 
+function openOverlay() {
+    document.getElementById("galleryOverlay").style.display = "block";
+  }
+  
+  function closeOverlay() {
+    document.getElementById("galleryOverlay").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+//   function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("slides");
+//     if (n > slides.length) {slideIndex = 1}
+//     if (n < 1) {slideIndex = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slides[slideIndex-1].style.display = "block";
+    
+//   }

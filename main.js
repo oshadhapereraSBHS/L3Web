ea08c4c9ae504751cf6f9a7e14d6f8260d2d16b5
oshadhapereraSBHS <80649645+oshadhapereraSBHS@users.noreply.github.com>
2023-07-21 21:04:40 +1200

    change logo img on hover

--- main.js
+++ main.js
@@ -89,10 +89,129 @@ function home() {
 }
 
 function hover1() {
-  document.getElementById("img1").src = "images/aboutUs3.jpg"
+  document.getElementById("img1").src = "images/aboutUs1Hover.jpg"
+  document.getElementById("img1").display.transform = "scale(2)";
 }
 
 function hover1Stop() {
   document.getElementById("img1").src = "images/aboutUs1.jpg"
 
+}
+
+function hover2() {
+  document.getElementById("img2").src = "images/aboutUs2Hover.jpg"
+}
+
+function hover2Stop() {
+  document.getElementById("img2").src = "images/aboutUs2.jpg"
+
+}
+
+function hover3() {
+  document.getElementById("img3").src = "images/aboutUs1Hover.jpg"
+}
+
+function hover3Stop() {
+  document.getElementById("img3").src = "images/aboutUs3.jpg"
+
+}
+
+function hover4() {
+  document.getElementById("img4").src = "images/aboutUs2Hover.jpg"
+}
+
+function hover4Stop() {
+  document.getElementById("img4").src = "images/aboutUs4.jpg"
+
+}
+
+function hover5() {
+  document.getElementById("img5").src = "images/aboutUs5Hover.jpg"
+}
+
+function hover5Stop() {
+  document.getElementById("img5").src = "images/aboutUs5.jpg"
+
+}
+
+function hover6() {
+  document.getElementById("img6").src = "images/aboutUs6Hover.jpg"
+}
+
+function hover6Stop() {
+  document.getElementById("img6").src = "images/aboutUs6.jpg"
+
+}
+
+function hover7() {
+  document.getElementById("img7").src = "images/aboutUs7Hover.jpg"
+}
+
+function hover7Stop() {
+  document.getElementById("img7").src = "images/aboutUs7.jpg"
+
+}
+
+function hover8() {
+  document.getElementById("img8").src = "images/aboutUs8Hover.jpg"
+}
+
+function hover8Stop() {
+  document.getElementById("img8").src = "images/aboutUs8.jpg"
+
+}
+
+
+
+
+  function contactSubmit(){ 
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    if(name = null || name == ""){
+      alert("Please enter your name");
+    } else if (email = null || email ==""){
+      alert("Please enter your email");
+    } else if (message = null || message ==""){
+      alert("Please enter your message");
+    } else {
+      var name = document.getElementById("name").value;
+
+      document.getElementById("contactForm").style.display = "none";
+      document.getElementById("contactReceived").style.display = "block";
+      document.getElementById("contactReceived").innerHTML = "Thank you " + name + ", we will get back to you as soon as possible.";
+    }
+    
+      
+    }
+    
+  
+
+    function signUpSubmit(){ 
+      var signUpName = document.getElementById("signUpName").value;
+      var signUpTutor = document.getElementById("signUpTutor").value;
+  
+      if(signUpName = null || signUpName == ""){
+        alert("Please enter your name");
+      } else if (signUpTutor = null || signUpTutor ==""){
+        alert("Please enter your tutor class");
+      } else {
+        var signUpName = document.getElementById("signUpName").value;
+  
+        document.getElementById("signUpForm").style.display = "none";
+        document.getElementById("signUpReceived").style.display = "block";
+        document.getElementById("signUpReceived").innerHTML = "Thank you for your interest in joining the chess club, " + signUpName + ". All you have to do now is turn up to chess club next Tuesday in 6P.";
+      }
+      
+        
+      }
+      
+    
+function logoChange1(){
+  document.getElementById("logo").src = "images/logo1.png";
+}
+
+function logoChange2(){
+  document.getElementById("logo").src = "images/logo.png";
 }

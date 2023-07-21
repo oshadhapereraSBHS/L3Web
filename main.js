341f1592a543db7966e5209fe66afe1b10cbc7e9
oshadhapereraSBHS <80649645+oshadhapereraSBHS@users.noreply.github.com>
2023-07-21 20:13:16 +1200

    contact form showing received message once submitted

--- main.js
+++ main.js
@@ -1,3 +1,175 @@
-function signUpPage (){
-    location.href = "signup.html";
+function signUpPage() {
+  location.href = "signup.html";
 }
+var dropdown1Clicks = 1;
+var dropdown2Clicks = 1;
+
+function dropdown1() {
+  dropdown2Clicks = 1;
+  dropdown1Clicks++;
+  if (dropdown1Clicks % 2 === 0) {
+    document.getElementById("dropdownItems1").style.display = "grid";
+    document.getElementById("overview").style.boxShadow = "-2px -2px 1px 0 black";
+    document.getElementById("forMembers").style.boxShadow = "none";
+    document.getElementById("forMembers").style.color = "#999"
+    document.getElementById("contact").style.color = "#999"
+    document.getElementById("dropdownItems2").style.display = "none";
+    document.getElementById("overview").style.color = "#073B4C"
+
+  } else {
+    document.getElementById("dropdownItems1").style.display = "none";
+    document.getElementById("forMembers").style.color = "#073B4C"
+    document.getElementById("overview").style.color = "#073B4C"
+    document.getElementById("contact").style.color = "#073B4C"
+    document.getElementById("overview").style.boxShadow = "none"
+  }
+}
+
+function dropdown2() {
+  dropdown1Clicks = 1;
+  dropdown2Clicks++;
+  if (dropdown2Clicks % 2 === 0) {
+
+    document.getElementById("dropdownItems2").style.display = "grid";
+    document.getElementById("dropdownItems1").style.display = "none";
+    document.getElementById("forMembers").style.boxShadow = "-2px -2px 1px 0 black"
+    document.getElementById("overview").style.boxShadow = "none";
+    document.getElementById("overview").style.color = "#999"
+    document.getElementById("contact").style.color = "#999"
+    document.getElementById("forMembers").style.color = "#073B4C"
+  } else {
+    document.getElementById("dropdownItems2").style.display = "none";
+    document.getElementById("overview").style.color = "#073B4C"
+    document.getElementById("forMembers").style.color = "#073B4C"
+    document.getElementById("contact").style.color = "#073B4C"
+    document.getElementById("forMembers").style.boxShadow = "none"
+
+  }
+}
+
+function openOverlay() {
+  document.getElementById("galleryOverlay").style.display = "block";
+}
+
+function closeOverlay() {
+  document.getElementById("galleryOverlay").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+
+}
+
+function home() {
+  location.href = "index.html";
+}
+
+function hover1() {
+  document.getElementById("img1").src = "images/aboutUs1Hover.jpg"
+}
+
+function hover1Stop() {
+  document.getElementById("img1").src = "images/aboutUs1.jpg"
+
+}
+
+function hover2() {
+  document.getElementById("img2").src = "images/aboutUs2Hover.jpg"
+}
+
+function hover2Stop() {
+  document.getElementById("img2").src = "images/aboutUs2.jpg"
+
+}
+
+function hover3() {
+  document.getElementById("img3").src = "images/aboutUs1Hover.jpg"
+}
+
+function hover3Stop() {
+  document.getElementById("img3").src = "images/aboutUs3.jpg"
+
+}
+
+function hover4() {
+  document.getElementById("img4").src = "images/aboutUs2Hover.jpg"
+}
+
+function hover4Stop() {
+  document.getElementById("img4").src = "images/aboutUs4.jpg"
+
+}
+
+function hover5() {
+  document.getElementById("img5").src = "images/aboutUs5Hover.jpg"
+}
+
+function hover5Stop() {
+  document.getElementById("img5").src = "images/aboutUs5.jpg"
+
+}
+
+function hover6() {
+  document.getElementById("img6").src = "images/aboutUs6Hover.jpg"
+}
+
+function hover6Stop() {
+  document.getElementById("img6").src = "images/aboutUs6.jpg"
+
+}
+
+function hover7() {
+  document.getElementById("img7").src = "images/aboutUs7Hover.jpg"
+}
+
+function hover7Stop() {
+  document.getElementById("img7").src = "images/aboutUs7.jpg"
+
+}
+
+function hover8() {
+  document.getElementById("img8").src = "images/aboutUs8Hover.jpg"
+}
+
+function hover8Stop() {
+  document.getElementById("img8").src = "images/aboutUs8.jpg"
+
+}
+
+
+
+
+  function contactSubmit(){ 
+    
+    var name = document.getElementById("name").value
+      document.getElementById("contactForm").style.display = "none";
+      document.getElementById("contactReceived").style.display = "block";
+      document.getElementById("contactReceived").innerHTML = "Thank you " + name + ", we will get back to you as soon as possible.";
+    }
+    
+  
+

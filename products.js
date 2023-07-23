ccdea91e822530bf3b90e29c9a8714d27db19cef
oshadhapereraSBHS <80649645+oshadhapereraSBHS@users.noreply.github.com>
2023-07-23 20:04:12 +1200

    calculate total items by adding quantities in cart

--- products.js
+++ products.js
@@ -22,135 +22,135 @@ window.onload = function () {
     if (localStorage.getItem("quantity3") == null) {
         document.getElementById("quantity3").value = 0
     }
-//     if (localStorage.getItem("quantity4") == null) {
-//         document.getElementById("quantity4").value = 0
-//     }
-//     if (localStorage.getItem("quantity5") == null) {
-//         document.getElementById("quantity5").value = 0
-//     }
-//     if (localStorage.getItem("quantity6") == null) {
-//         document.getElementById("quantity6").value = 0
-//     }
+    if (localStorage.getItem("quantity4") == null) {
+        document.getElementById("quantity4").value = 0
+    }
+    if (localStorage.getItem("quantity5") == null) {
+        document.getElementById("quantity5").value = 0
+    }
+    if (localStorage.getItem("quantity6") == null) {
+        document.getElementById("quantity6").value = 0
+    }
 
 
-// }
+}
 
 
 
 
-// function minus1() {
-//     //if user have added items to cart (so quantity is > 0)
-//     if (localStorage.getItem("quantity1") > 0) {
-//         //reduce quantity by 1
-//         document.getElementById("quantity1").stepDown();
-//         //store new value as quantity
-//         localStorage.setItem("quantity1", document.getElementById("quantity1").value)
-//     }
-// }
+function minus1() {
+    //if user have added items to cart (so quantity is > 0)
+    if (localStorage.getItem("quantity1") > 0) {
+        //reduce quantity by 1
+        document.getElementById("quantity1").stepDown();
+        //store new value as quantity
+        localStorage.setItem("quantity1", document.getElementById("quantity1").value)
+    }
+}
 
-// function plus1() {
-//     //increase quantity by 1
-//     document.getElementById("quantity1").stepUp();
-//     //store new value as quantity
-//     localStorage.setItem("quantity1", document.getElementById("quantity1").value)
+function plus1() {
+    //increase quantity by 1
+    document.getElementById("quantity1").stepUp();
+    //store new value as quantity
+    localStorage.setItem("quantity1", document.getElementById("quantity1").value)
 
-// }
+}
 
-// //above repeated for every product
-// function minus2() {
-//     if (localStorage.getItem("quantity2") > 0) {
-//         document.getElementById("quantity2").stepDown();
-//         localStorage.setItem("quantity2", document.getElementById("quantity2").value)
+//above repeated for every product
+function minus2() {
+    if (localStorage.getItem("quantity2") > 0) {
+        document.getElementById("quantity2").stepDown();
+        localStorage.setItem("quantity2", document.getElementById("quantity2").value)
 
-//     }
+    }
 
-// }
+}
 
-// function plus2() {
-//     document.getElementById("quantity2").stepUp();
-//     localStorage.setItem("quantity2", document.getElementById("quantity2").value)
+function plus2() {
+    document.getElementById("quantity2").stepUp();
+    localStorage.setItem("quantity2", document.getElementById("quantity2").value)
 
-// }
-
-
-// function minus3() {
-//     if (localStorage.getItem("quantity3") > 0) {
-//         document.getElementById("quantity3").stepDown();
-//         localStorage.setItem("quantity3", document.getElementById("quantity3").value)
-
-//     }
-
-// }
-
-// function plus3() {
-
-//     document.getElementById("quantity3").stepUp();
-//     localStorage.setItem("quantity3", document.getElementById("quantity3").value)
-
-// }
-
-// function minus4() {
-//     if (localStorage.getItem("quantity4") > 0) {
-
-//         document.getElementById("quantity4").stepDown();
-//         localStorage.setItem("quantity4", document.getElementById("quantity4").value)
-
-//     }
-// }
-
-// function plus4() {
-//     document.getElementById("quantity4").stepUp();
-//     localStorage.setItem("quantity4", document.getElementById("quantity4").value)
+}
 
 
-// }
+function minus3() {
+    if (localStorage.getItem("quantity3") > 0) {
+        document.getElementById("quantity3").stepDown();
+        localStorage.setItem("quantity3", document.getElementById("quantity3").value)
 
-// function minus5() {
-//     if (localStorage.getItem("quantity5") > 0) {
-//         document.getElementById("quantity5").stepDown();
-//         localStorage.setItem("quantity5", document.getElementById("quantity5").value)
+    }
 
-//     }
+}
 
-// }
+function plus3() {
 
-// function plus5() {
+    document.getElementById("quantity3").stepUp();
+    localStorage.setItem("quantity3", document.getElementById("quantity3").value)
 
-//     document.getElementById("quantity5").stepUp();
-//     localStorage.setItem("quantity5", document.getElementById("quantity5").value)
+}
 
-// }
+function minus4() {
+    if (localStorage.getItem("quantity4") > 0) {
 
-// function minus6() {
-//     if (localStorage.getItem("quantity6") > 0) {
-//         document.getElementById("quantity6").stepDown();
-//         localStorage.setItem("quantity6", document.getElementById("quantity6").value)
+        document.getElementById("quantity4").stepDown();
+        localStorage.setItem("quantity4", document.getElementById("quantity4").value)
 
-//     }
+    }
+}
 
-// }
-
-// function plus6() {
-//     document.getElementById("quantity6").stepUp();
-//     localStorage.setItem("quantity6", document.getElementById("quantity6").value)
+function plus4() {
+    document.getElementById("quantity4").stepUp();
+    localStorage.setItem("quantity4", document.getElementById("quantity4").value)
 
 
-// }
+}
+
+function minus5() {
+    if (localStorage.getItem("quantity5") > 0) {
+        document.getElementById("quantity5").stepDown();
+        localStorage.setItem("quantity5", document.getElementById("quantity5").value)
+
+    }
+
+}
+
+function plus5() {
+
+    document.getElementById("quantity5").stepUp();
+    localStorage.setItem("quantity5", document.getElementById("quantity5").value)
+
+}
+
+function minus6() {
+    if (localStorage.getItem("quantity6") > 0) {
+        document.getElementById("quantity6").stepDown();
+        localStorage.setItem("quantity6", document.getElementById("quantity6").value)
+
+    }
+
+}
+
+function plus6() {
+    document.getElementById("quantity6").stepUp();
+    localStorage.setItem("quantity6", document.getElementById("quantity6").value)
 
 
-// function updateQuantity1() {
-//     //when add to cart button is pressed, store the value entered by user
-//     localStorage.setItem("quantity1", document.getElementById("quantity1").value)
-// }
+}
 
-// //repeat above for every product
-// function updateQuantity2() {
-//     localStorage.setItem("quantity2", document.getElementById("quantity2").value)
-// }
 
-// function updateQuantity3() {
-//     localStorage.setItem("quantity3", document.getElementById("quantity3").value)
-// }
+function updateQuantity1() {
+    //when add to cart button is pressed, store the value entered by user
+    localStorage.setItem("quantity1", document.getElementById("quantity1").value)
+}
+
+//repeat above for every product
+function updateQuantity2() {
+    localStorage.setItem("quantity2", document.getElementById("quantity2").value)
+}
+
+function updateQuantity3() {
+    localStorage.setItem("quantity3", document.getElementById("quantity3").value)
+}
 
 function updateQuantity4() {
     localStorage.setItem("quantity4", document.getElementById("quantity4").value)
@@ -178,13 +178,13 @@ function updateItemNumber() {
         localStorage.getItem("quantity5") * 1 +
         localStorage.getItem("quantity6") * 1);
 
-    if (totalItems > 0 && totalItems != null) {
-        //if total item number is above 0 or null, show item number beside cart
-        document.getElementById("itemNumber").style.display = "block";
-        document.getElementById("itemNumber").innerHTML = totalItems;
-    } else {
-        //if total item number is below 0, don't show item number beside cart
-        document.getElementById("itemNumber").style.display = "none";
-    }
+//     if (totalItems > 0 && totalItems != null) {
+//         //if total item number is above 0 or null, show item number beside cart
+//         document.getElementById("itemNumber").style.display = "block";
+//         document.getElementById("itemNumber").innerHTML = totalItems;
+//     } else {
+//         //if total item number is below 0, don't show item number beside cart
+//         document.getElementById("itemNumber").style.display = "none";
+//     }
 
-}
+// }
